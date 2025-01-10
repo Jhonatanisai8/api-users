bb2bc831837cdebbf2f7a519595687060cf31006
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 22:17:45 -0500

    implementacion del metodo modificar un User (PUT-MAPPING) en la clase UserController

--- src/main/java/org/isai/api/api_users/controllers/UserController.java
+++ src/main/java/org/isai/api/api_users/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,12 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public Optional<UserModel> getUserByID(@PathVariable Long id){
+    public Optional<UserModel> getUserByID(@PathVariable Long id) {
         return this.service.getUserByID(id);
+    }
+
+    @PutMapping("/{id}")
+    public UserModel updatUserByID(@RequestBody UserModel userRequest, Long id) {
+        return this.service.updateUserByID(userRequest, id);
     }
 }
