a553fab991122b7e5e07b4ed503244af8f576996
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 21:31:26 -0500

    configuracion inicial

---
+++ src/test/java/org/isai/api/api_users/ApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isai.api.api_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
