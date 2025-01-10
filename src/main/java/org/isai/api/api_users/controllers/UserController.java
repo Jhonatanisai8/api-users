c5351a9fa8de6c074b42dcb6bfeaeac68a466509
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 22:25:54 -0500

    implementacion del metodo eliminar un User (DELETE-MAPPING) en la clase UserController

--- src/main/java/org/isai/api/api_users/controllers/UserController.java
+++ src/main/java/org/isai/api/api_users/controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.isai.api.api_users.models.UserModel;
 import org.isai.api.api_users.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,7 +38,17 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public UserModel updatUserByID(@RequestBody UserModel userRequest, Long id) {
+    public UserModel updatUserByID(@RequestBody UserModel userRequest, @PathVariable Long id) {
         return this.service.updateUserByID(userRequest, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUserByID(@PathVariable Long id){
+        boolean delete = this.service.deleteUserByID(id);
+        if (delete) {
+            return "User Delete ID: "+id;
+        } else {
+            return "User not content ID: "+id;
+        }
     }
 }
