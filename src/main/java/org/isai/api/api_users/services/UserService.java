0d1465226411b862e6af965bca579c228451c640
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 21:50:43 -0500

    implementacion del metodo getUsers en la clase UserService

--- src/main/java/org/isai/api/api_users/services/UserService.java
+++ src/main/java/org/isai/api/api_users/services/UserService.java
@@ -1,5 +1,20 @@
 package org.isai.api.api_users.services;
 
+import java.util.List;
+
+import org.isai.api.api_users.models.UserModel;
+import org.isai.api.api_users.repositories.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
-    
+
+    @Autowired
+    IUserRepository repository;
+
+    public List<UserModel> getUsers() {
+        return repository.findAll();
+    }
+
 }
