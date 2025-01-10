67f6eebbe858d25304807947a90e0d6f0923d48c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 21:43:35 -0500

    mapeo de la entidad user

--- src/main/java/org/isai/api/api_users/models/UserModel.java
+++ src/main/java/org/isai/api/api_users/models/UserModel.java
@@ -1,5 +1,59 @@
 package org.isai.api.api_users.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class UserModel {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firtName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirtName() {
+        return firtName;
+    }
+
+    public void setFirtName(String firtName) {
+        this.firtName = firtName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
