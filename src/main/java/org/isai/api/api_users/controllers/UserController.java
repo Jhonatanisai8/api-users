9d26292e05bb80b13932bfeeaf71a3ece6266d1c
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 22:03:46 -0500

    implementacion del metodo obtener un USER (GET-MAPPING) en la clase UserController

--- src/main/java/org/isai/api/api_users/controllers/UserController.java
+++ src/main/java/org/isai/api/api_users/controllers/UserController.java
@@ -1,11 +1,13 @@
 package org.isai.api.api_users.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.isai.api.api_users.models.UserModel;
 import org.isai.api.api_users.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +28,10 @@ public class UserController {
     @PostMapping
     public UserModel savUser(@RequestBody UserModel user) {
         return this.service.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<UserModel> getUserByID(@PathVariable Long id){
+        return this.service.getUserByID(id);
     }
 }
