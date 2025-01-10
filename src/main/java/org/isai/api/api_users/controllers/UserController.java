ca517caa44d90d46016a044172a89b861844d0dc
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2025-01-09 21:59:54 -0500

    implementacion del metodo guardar un USER (POST-MAPPING) en la clase UserController

--- src/main/java/org/isai/api/api_users/controllers/UserController.java
+++ src/main/java/org/isai/api/api_users/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.isai.api.api_users.models.UserModel;
 import org.isai.api.api_users.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +23,8 @@ public class UserController {
         return this.service.getUsers();
     }
 
+    @PostMapping
+    public UserModel savUser(@RequestBody UserModel user) {
+        return this.service.saveUser(user);
+    }
 }
